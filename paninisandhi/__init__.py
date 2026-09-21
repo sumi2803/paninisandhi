e9a668989d43bb7f87be1b2d7353e57fbc1ac9ad
sumi2803 <sumithra2803@gmail.com>
2026-09-21 14:49:12 +0530

    Include pratyahara functions in module exports
    Added pratyahara functions and constants to __all__.

--- paninisandhi/__init__.py
+++ paninisandhi/__init__.py
@@ -1,4 +1,8 @@
 from .rules import yan_sandhi, is_vriddhi
 from .data import VOWELS, YAN, MATRA, VRIDDHI
+from .pratyahara import SUTRAS, count_pratyaharas, generate_pratyaharas
 
-__all__ = ["yan_sandhi", "is_vriddhi", "VOWELS", "YAN", "MATRA", "VRIDDHI"]
+__all__ = [
+    "yan_sandhi", "is_vriddhi", "VOWELS", "YAN", "MATRA", "VRIDDHI",
+    "SUTRAS", "count_pratyaharas", "generate_pratyaharas",
+]
